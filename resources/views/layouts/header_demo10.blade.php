9706f67f5c9862b8300874f35916d1b0795befc3
AbdallahSaeed0 <bodyahly17@gmail.com>
2026-02-08 16:26:55 +0200

    remove from header10

--- resources/views/layouts/header_demo10.blade.php
+++ resources/views/layouts/header_demo10.blade.php
@@ -210,21 +210,6 @@ $chunk= App\Models\Service::get()->take(3);
     
     <!-- Main CSS File -->
  
-<!-- Snap Pixel Code -->
-<script type='text/javascript'>
-(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
-{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
-a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
-r.src=n;var u=t.getElementsByTagName(s)[0];
-u.parentNode.insertBefore(r,u);})(window,document,
-'https://sc-static.net/scevent.min.js');
-
-snaptr('init', '69b9f581-4e2d-4f35-8239-e1eaa687531d', {});
-
-snaptr('track', 'PAGE_VIEW');
-
-</script>
-<!-- End Snap Pixel Code -->
 
 <script>
   // Add to Cart Event
